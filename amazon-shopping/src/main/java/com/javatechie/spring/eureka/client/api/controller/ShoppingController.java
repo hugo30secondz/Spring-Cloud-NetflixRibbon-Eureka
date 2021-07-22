cd24c2d09037293f86506d1e689498c3fedb3e4f
hugo30secondz <42413399+hugo30secondz@users.noreply.github.com>
2021-07-21 19:25:47 -0500

    First commit

---
+++ amazon-shopping/src/main/java/com/javatechie/spring/eureka/client/api/controller/ShoppingController.java
@@ -0,0 +1,19 @@
+package com.javatechie.spring.eureka.client.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ShoppingController {
+	
+	@Autowired
+	private RestTemplate template;
+	
+	@GetMapping("/amazon-payment/{price}")
+	public String invokePaymentService(@PathVariable int price) {
+		return template.getForObject("http://PAYMENT-SERVICE/payment-provider/payNow/"+price, String.class);
+	}
+}
